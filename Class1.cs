b3179cc9cdd691a4f93b4a6f9a2a436a08167af2
volcogot <volcogot@mail.ru>
2020-12-28 23:58:52 +0300

    FirstCommit

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    public class exponentiation
+    {
+        public string a, b;
+        public exponentiation(string a, string b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public string ogo()
+        {
+            return "" + Math.Pow(Double.Parse(a),Double.Parse(b));
+        }
+        public string Clear()
+        {
+            return "";
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Class1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+       public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                exponentiation exp = new exponentiation(tb1.Text, tb2.Text);
+                otvet.Content = exp.ogo();
+            }
+            catch
+            {
+                otvet.Content = "Error";
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tb1.Clear();
+            tb2.Clear();
+            exponentiation Clear = new exponentiation(tb1.Text, tb2.Text);
+            otvet.Content = Clear.Clear();
+        }
+    }
+}
